0211b12eff61d6ed4bacbf36bccfb71cb56c9062
Dennis Buijs <dennisdennisbuijs@gmail.com>
2024-05-31 00:36:18 +0200

    style: add system font

--- src/Infrastructure/Controller/Web/Template/header.php
+++ src/Infrastructure/Controller/Web/Template/header.php
@@ -4,6 +4,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>PHP Framework</title>
+
+        <link rel="stylesheet" href="http://localhost:8000/public/style.css">
     </head>
     <body>
         <header><h1>PHP Framework</h1></header>
